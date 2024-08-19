d9948636902054b3b06e62a724197c734c52cb68
Gavriel Linoy <kraserultra@gmail.com>
2024-08-19 23:31:14 +0300

    delete and comment stuff

--- src/node.py
+++ src/node.py
@@ -8,36 +8,34 @@ class Node:
         self.__addr = (ip, port)
 
     def run(self) -> None:
-        flag = False
-        
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.socket:
                 self.socket.bind(self.__addr)
                 self.socket.listen()
-                client_socket, client_addr = self.socket.accept()
+                prev_socket, prev_addr = self.socket.accept()
                 print(self.__addr, "is running...")
 
                 if not len(self.next):
-                    with client_socket:
-                        print(f"Connected by {client_addr}")
-                        data = client_socket.recv(1024)
+                    with prev_socket:
+                        print(f"Connected by {prev_addr}")
+                        data = prev_socket.recv(1024)
 
                         data = data.decode()
                         print(data)
                         self.next = (data.split(',')[0], data.split(',')[1])
 
                         msg = f"ok next {self.next} updated"
-                        client_socket.sendall(msg.encode())
-            if flag:
+                        prev_socket.sendall(msg.encode())
+                        
                 # TODO: back to listening
-                while True:
-                    data = client_socket.recv(1024)
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as next_socket:
-                        next_socket.connect(self.next)
-                        next_socket.sendall(data)
-                        data = next_socket.recv(1024)
-                        client_socket.sendall(data)
-                        print("new sock")
+                # while True:
+                #     data = prev_socket.recv(1024)
+                #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as next_socket:
+                #         next_socket.connect(self.next)
+                #         next_socket.sendall(data)
+                #         data = next_socket.recv(1024)
+                #         prev_socket.sendall(data)
+                #         print("new sock")
                     
         except BaseException as be:
             print(be)
