00dcf190ce100a0813c5cc9c1cf463a25a83f2c0
Gavriel Linoy <kraserultra@gmail.com>
2024-08-20 18:54:50 +0300

    Add test for AES basic encryption and decryption + add cryptography lib to requirements

---
+++ tests/aes_test.py
@@ -0,0 +1,19 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+# Pseudo-random key and initialisation vector
+key = os.urandom(32)           # (32*8=256-bit. AES also accepts 128/192-bit)
+print(f"key: {key}")
+init_vector = os.urandom(16)   # (16*8=128-bit. AES only accepts this size)
+print(f"initialization vector: {init_vector}")
+
+# Setup module-specific classes
+cipher = Cipher(algorithms.AES(key), modes.GCM(init_vector))
+encryptor = cipher.encryptor()
+decryptor = cipher.decryptor()
+
+# Encrypt and decrypt data
+chipertext = encryptor.update(b"a secret message") + encryptor.finalize()
+print(chipertext)
+plaintext = decryptor.update(chipertext) # + decryptor.finalize()
+print(plaintext) # 'a secret message
