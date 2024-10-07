ac714d4be7fc6aa6ae02fedc46723b8c977df6b7
Gavriel Linoy <kraserultra@gmail.com>
2024-10-07 21:08:05 +0300

    Fix install_boost.bat again

--- src/install_boost.bat
+++ src/install_boost.bat
@@ -4,8 +4,6 @@ REM Donwload Boost Source Code
 curl -O https://archives.boost.io/release/1.86.0/source/boost_1_86_0.zip
 tar -xf "boost_1_86_0.zip"
 del /f /q "boost_1_86_0.zip"
-cd "./boost_1_86_0"
-cd ..
 
 REM Download B2
 curl -LO https://github.com/bfgroup/b2/releases/download/5.2.1/b2-5.2.1.zip
