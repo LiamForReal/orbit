682ca54469726672e17f6221048fcc35144f7ae7
theRealLiam <liamneir11@gmail.com>
2024-10-18 21:59:42 +0300

    the server now waits to the client msg but somehow doesnt get it

--- src/networking/client.cpp
+++ src/networking/client.cpp
@@ -1,4 +1,3 @@
-#include "client.h"
 #include <iostream>
 #include <array>
 #include <boost/asio.hpp>
@@ -21,36 +20,41 @@ int main(int argc, char* argv[])
         boost::system::error_code error;
 
         int amountOfNodes = 0, pathLength = 0; 
-        do{
+        do {
             std::cout << "Enter amount of nodes you want to use: ";
             std::cin >> amountOfNodes;
             std::cout << "Enter path length nodes you want to use: ";
             std::cin >> pathLength;
-        }while(amountOfNodes >= 4 || amountOfNodes <= 0);
+        } while (amountOfNodes >= 4 || amountOfNodes <= 0);
 
-        std::string message = std::to_string(amountOfNodes) + " " +  std::to_string(pathLength); //msg
+        std::string message = std::to_string(amountOfNodes) + " " +  std::to_string(pathLength) + "\n"; //msg
 
-        boost::system::error_code ignored_error;
-        boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        boost::asio::write(socket, boost::asio::buffer(message), error);
+        if (error)
+        {
+            std::cerr << "Error writing to server: " << error.message() << std::endl;
+            return 1;
+        }
 
         size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
         if (error == boost::asio::error::eof)
         {
-            throw boost::system::system_error(error); // Some other error.
+            std::cout << "Connection closed by server.\n";
         }
         else if (error)
         {
-            throw boost::system::system_error(error); // Some other error.
+            std::cerr << "Error reading from server: " << error.message() << std::endl;
+            return 1;
         }
 
+        std::cout << "Response from server: ";
         std::cout.write(buf.data(), len);
+        std::cout << std::endl;
     }
     catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
 
-    getchar();
     return 0;
 }
--- src/networking/tcpServer.h
+++ src/networking/tcpServer.h
@@ -1,57 +1,59 @@
 #pragma once
 
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <boost/asio.hpp>
 #include "tcpConnection.hpp"
 #include <map>
 #include <vector>
 #include <utility>
-using boost::asio::ip::tcp;
 
 class tcp_server
 {
-public:
-    tcp_server(boost::asio::io_context& io_context)
-        : io_context_(io_context),
-        acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
-    {
-        std::cout << "Server binding to port 13...\n";
-        start_accept();
-    }
-private:
-    void start_accept()
-    {
-        std::cout << "Server waiting for connection...\n";
-        tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
-        acceptor_.async_accept(new_connection->socket(),
-            std::bind(&tcp_server::handle_accept, this, new_connection,
-            boost::asio::placeholders::error));
-    }
-
-    void handle_accept(tcp_connection::pointer new_connection,
-        const boost::system::error_code& error)
-    {
-        if (!error)
+    public:
+        tcp_server(boost::asio::io_context& io_context)
+            : io_context(io_context),
+            acceptor(io_context, tcp::endpoint(tcp::v4(), 13)),
+            is_client_being_served(false)
         {
-            std::cout << "Client connected.\n";
-            new_connection->start();
+            std::cout << "Server binding to port 13...\n";
+            start_accept();
         }
-        else
-        {
-            std::cerr << "Error accepting connection: " << error.message() << std::endl;
-        }
-        start_accept(); // Continue accepting connections
-    }
 
-    boost::asio::io_context& io_context_;
-    tcp::acceptor acceptor_;
+    private:
+        void start_accept()
+        {
+            if (!is_client_being_served) {
+                std::cout << "Server waiting for connection...\n";
+                tcp_connection::pointer new_connection = tcp_connection::create(io_context);
+                acceptor.async_accept(new_connection->socket(),
+                    std::bind(&tcp_server::handle_accept, this, new_connection,
+                    boost::asio::placeholders::error));
+            }
+        }
+
+        void handle_accept(tcp_connection::pointer new_connection, const boost::system::error_code& error)
+        {
+            if (!error)
+            {
+                std::cout << "Client connected.\n";
+                is_client_being_served = true;  // Pause accepting other clients
+                new_connection->start([this]() {
+                    is_client_being_served = false;  // Resume accepting clients
+                    start_accept();
+                });
+            }
+            else
+            {
+                std::cerr << "Error accepting connection: " << error.message() << std::endl;
+                start_accept();  // Resume accepting connections even on error
+            }
+        }
+
+        boost::asio::io_context& io_context;
+        tcp::acceptor acceptor;
+        bool is_client_being_served;
 };
 
-/*  
+/*
     std::map<int, tcp::socket> sockets;
-    std::map<int, std::vector<tcp::socket> paths; 
+    std::map<int, std::vector<tcp::socket>> paths;
     std::map<tcp::socket, std::chrono::high_resolution_clock::time_point> responseTime;
 */
