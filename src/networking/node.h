08c67c0dd8e7089848f1c94b90c347f09e95aa2c
liam meir <liamneir11@gmail.com>
2024-11-11 12:44:34 +0200

    succeeded

--- src/networking/node.h
+++ src/networking/node.h
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <thread>
 #include <chrono> 
-
+#define COMMUNICATE_NODE_PORT 9050
+#define CONTROL_NODE_PORT 9051
+#define COMMUNICATE_SERVER_PORT 9787
 class node{
 
 };
