2645f7fff0d4601113eadb16bdeea427b888c418
Gavriel Linoy <kraserultra@gmail.com>
2024-11-15 15:04:04 +0200

    Do deserialization (excluding RSA and ECDHE)

--- src/utils/DeserializerRequests.cpp
+++ src/utils/DeserializerRequests.cpp
@@ -34,26 +34,153 @@ NodeOpenRequest DeserializerRequests::deserializeNodeOpeningRequest(const std::v
 	{
 		throw std::runtime_error("Invalid json structure passed");
 	}
+
 	return request;
 }
 
 LinkRequest DeserializerRequests::deserializeLinkRequest(const std::vector<unsigned char>& buffer)
 {
+	LinkRequest request;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
 
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		request.circuit_id = jsonData["circuit_id"];
+		request.nextNode = jsonData["nextNode"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return request;
 }
+
 HttpGetRequest DeserializerRequests::deserializeHttpGetRequest(const std::vector<unsigned char>& buffer)
 {
+	HttpGetRequest request;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
 
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		request.circuit_id = jsonData["circuit_id"];
+		request.msg = jsonData["msg"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return request;
 }
+
 CloseConnectionRequest DeserializerRequests::deserializeCloseConnectionRequest(const std::vector<unsigned char>& buffer)
 {
+	CloseConnectionRequest request;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
 
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		request.circuit_id = jsonData["circuit_id"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return request;
 }
+
 DeleteCircuitRequest DeserializerRequests::deserializeDeleteCircuitRequest(const std::vector<unsigned char>& buffer)
 {
+    DeleteCircuitRequest request;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
     
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		request.circuit_id = jsonData["circuit_id"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return request;
 }
+
 GetDomainRequest DeserializerRequests::deserializeGetDomainRequest(const std::vector<unsigned char>& buffer)
 {
+	GetDomainRequest request;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
 
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		request.domain = jsonData["domain"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return request;
 }
--- src/utils/DeserializerResponses.cpp
+++ src/utils/DeserializerResponses.cpp
@@ -1,35 +1,184 @@
 #include "DeserializerResponses.h"
 
-    RsaKeyExchangeResponse DeserializerResponses::deserializeRsaKeyExchangeResponse(const std::vector<unsigned char>& buffer)
-    {
+RsaKeyExchangeResponse DeserializerResponses::deserializeRsaKeyExchangeResponse(const std::vector<unsigned char>& buffer)
+{
+}
 
-    }
-    EcdheKeyExchangeResponse DeserializerResponses::deserializeEcdheKeyExchangeResponse(const std::vector<unsigned char>& buffer)
-    {
+EcdheKeyExchangeResponse DeserializerResponses::deserializeEcdheKeyExchangeResponse(const std::vector<unsigned char>& buffer)
+{
+}
 
-    }
-    NodeOpenResponse DeserializerResponses::deserializeNodeOpeningResponse(const std::vector<unsigned char>& buffer)
-    {
+NodeOpenResponse DeserializerResponses::deserializeNodeOpeningResponse(const std::vector<unsigned char>& buffer)
+{
+    NodeOpenResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
 
-    }
-    LinkResponse DeserializerResponses::deserializeLinkResponse(const std::vector<unsigned char>& buffer)
-    {
-
-    }
-    HttpGetResponse DeserializerResponses::deserializeHttpGetResponse(const std::vector<unsigned char>& buffer)
-    {
-
-    }
-    CloseConnectionResponse DeserializerResponses::deserializeCloseConnectionResponse(const std::vector<unsigned char>& buffer)
-    {
-
-    }
-    DeleteCircuitResponse DeserializerResponses::deserializeDeleteCircuitResponse(const std::vector<unsigned char>& buffer)
-    {
-        
-    }
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
     
-    GetDomainResponse DeserializerResponses::deserializeGetDomainResponse(const std::vector<unsigned char>& buffer)
-    {
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
 
-    }
+	try
+	{
+		response.status = jsonData["status"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;
+}
+
+LinkResponse DeserializerResponses::deserializeLinkResponse(const std::vector<unsigned char>& buffer)
+{
+    LinkResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		response.status = jsonData["status"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;
+}
+
+HttpGetResponse DeserializerResponses::deserializeHttpGetResponse(const std::vector<unsigned char>& buffer)
+{
+    HttpGetResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		response.status = jsonData["status"];
+        response.content = jsonData["content"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;
+}
+
+CloseConnectionResponse DeserializerResponses::deserializeCloseConnectionResponse(const std::vector<unsigned char>& buffer)
+{
+    CloseConnectionResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		response.status = jsonData["status"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;
+}
+
+DeleteCircuitResponse DeserializerResponses::deserializeDeleteCircuitResponse(const std::vector<unsigned char>& buffer)
+{
+    DeleteCircuitResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		response.status = jsonData["status"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;    
+}
+
+GetDomainResponse DeserializerResponses::deserializeGetDomainResponse(const std::vector<unsigned char>& buffer)
+{
+    GetDomainResponse response;
+	std::string jsonDataStr = "";
+	unsigned int len = 0, i = 0;
+
+	std::memcpy(&len, buffer.data() + INC, BYTES_TO_COPY);
+	std::cout << len << std::endl;
+    
+    for (i = 0; i < len; i++)
+	{
+		jsonDataStr += buffer[BYTES_TO_COPY + INC + i];
+	}
+    
+	std::cout << jsonDataStr;
+	json jsonData = json::parse(jsonDataStr);
+
+	try
+	{
+		response.status = jsonData["status"];
+	}
+	catch (...)
+	{
+		throw std::runtime_error("Invalid json structure passed");
+	}
+	
+	return response;
+}
