5a5af0f5aaaa7691045f1784f7813a4a2b3416d5
theRealLiam <liamneir11@gmail.com>
2025-01-09 19:03:24 +0200

    done making ECDHE set up need to add tester

--- src/utils/ECDHE.cpp
+++ src/utils/ECDHE.cpp
@@ -1,17 +1,51 @@
 #include "ECDHE.h"
 
 //create 
-ECDHE::ECDHE() 
-{}
-ECDHE::~ECDHE() 
-{}
-std::pair<uint256_t, std::pair<uint256_t, uint256_t>> createInfo() 
-{}
-uint256_t ECDHE::getRandomPrimeNumberByRange(uint256_t lower_bound = 1, uint256_t upper_bound = 1) 
-{}
-uint256_t ECDHE::createP() 
-{}
-uint256_t ECDHE::createG() 
-{}
-uint256_t ECDHE::createKey() 
-{}
+ECDHE::ECDHE() {} // nothing to construct
+
+ECDHE::~ECDHE() {} // nothing to diconstruct
+
+std::pair<uint256_t, std::pair<uint256_t, uint256_t>> ECDHE::createInfo()
+{
+	std::promise<uint256_t> promiseG;
+	std::promise<uint256_t> promiseP;
+
+	std::future<uint256_t> futurePromiseG = promiseG.get_future();
+	std::future<uint256_t> futurePromiseP = promiseP.get_future();
+
+
+	std::thread generateQThread(&ECDHE::createElement, this, std::move(promiseG));
+	std::thread generatePThread(&ECDHE::createElement, this, std::move(promiseP));
+	
+	uint256_t tmpKey = createTmpKey();
+
+	generateQThread.join();
+	generatePThread.join();
+	
+	std::pair<uint256_t, uint256_t> gAndP = std::make_pair<uint256_t, uint256_t>(futurePromiseG.get(), futurePromiseP.get());
+	return std::pair<uint256_t, std::pair<uint256_t, uint256_t>>(tmpKey, gAndP);
+}
+
+uint256_t ECDHE::createTmpKey()
+{
+	uint256_t lowerBoned = uint256_t(1);
+	uint256_t upperBoned = (uint256_t(1) << 256) - 1;
+	return getRandomPrimeNumberByRange(lowerBoned, upperBoned);
+}
+
+uint256_t ECDHE::getRandomPrimeNumberByRange(uint256_t lower_bound, uint256_t upper_bound) 
+{
+	return prime_numbers_manager::getRandomPrimeNumber<uint256_t>(lower_bound, upper_bound);
+}
+
+uint256_t ECDHE::createElement(std::promise<uint256_t>&& promise)
+{
+	uint256_t lowerBoned = uint256_t(1) << 128;
+	uint256_t upperBoned = (uint256_t(1) << 256) - 1;
+	return getRandomPrimeNumberByRange(lowerBoned, upperBoned);
+}
+
+uint256_t ECDHE::createDefiKey(uint256_t base, uint256_t tmpKey, uint256_t moduler)
+{
+	return mod_exp<uint256_t>((cpp_int)(base), tmpKey, moduler);
+}
--- src/utils/ECDHE.h
+++ src/utils/ECDHE.h
@@ -1,18 +1,21 @@
 #pragma once 
 #include "prime_numbers_manager.h"
+#include "utils.hpp"
+#include <thread>
+#include <future>
 class ECDHE
 {
 public:
 	//create 
 	ECDHE();
 	~ECDHE();
-	std::pair<uint256_t, std::pair<uint256_t, uint256_t>> createInfo();
+	std::pair<uint256_t, std::pair<uint256_t, uint256_t>> createInfo(); //side a
+	uint256_t createTmpKey(); //side b
+	uint256_t createDefiKey(uint256_t base, uint256_t tmpKey, uint256_t moduler);
 private:
 	uint256_t getRandomPrimeNumberByRange(uint256_t lower_bound = 1, uint256_t upper_bound = 1);
-	uint256_t createP();
-	uint256_t createG();
-	uint256_t createKey();
-	//baser - g, moduler - p : those are two public prims between 128 - 256 bits(size 256)
+	uint256_t createElement(std::promise<uint256_t>&& promise);
+	//base- g, moduler - p : those are two public prims between 128 - 256 bits(size 256)
 	//side1 key - a, side2 key - b : those are two private prims between 1 - 256 bits(size 256)
 	//the shered seacret(the aes key of both side1 and side2) - value between 128 - 256 bits(more close to 256) (size 256)
 
